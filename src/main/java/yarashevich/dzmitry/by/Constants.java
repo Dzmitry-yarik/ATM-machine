f1743073433ca6cb53c504eeac3948e543d769f9
Dzmitry-yarik <dimon_yarik@mail.ru>
2024-07-12 07:31:12 +0300

    Fix time

--- src/main/java/yarashevich/dzmitry/by/Constants.java
+++ src/main/java/yarashevich/dzmitry/by/Constants.java
@@ -3,5 +3,5 @@ package yarashevich.dzmitry.by;
 public class Constants {
     public static final String FILE_PATH = "src/main/resources/data.txt";
     public static final int MAX_FAILED_ATTEMPTS = 3;
-    public static final long UNBLOCK_DELAY = 2;
+    public static final long UNBLOCK_DELAY = 1;
 }
